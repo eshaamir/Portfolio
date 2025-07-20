a64db0332c4887f77a9e13c804b5782738c922dc
Esha <eshaamirgondal@gmail.com>
2025-07-20 14:51:50 +0500

    Initial commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,22 +1,24 @@
-import React from 'react';
-import { motion } from 'framer-motion';
-import { styles } from '../styles';
-import { services } from '../constants';
-import { fadeIn, textVariant } from '../utils/motion';
-import { SectionWrapper } from '../hoc';
+import React from "react";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
 
 const ServiceCard = ({ index, title, icon }) => {
   return (
     <motion.div
-      variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
-      className="xs:w-[250px] w-full card-gradient p-[1px] rounded-[20px] shadow-card">
+      variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+      className="xs:w-[250px] w-full card-gradient p-[1px] rounded-[20px] shadow-card"
+    >
       <div
         options={{
           max: 45,
           scale: 1,
           speed: 450,
         }}
-        className="bg-jetLight rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+        className="bg-jetLight rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+      >
         <img src={icon} alt={title} className="w-16 h-16 object-contain" />
         <h3 className="text-taupe text-[18px] font-bold text-center">
           {title}
@@ -35,14 +37,20 @@ const About = () => {
       </motion.div>
 
       <motion.p
-        variants={fadeIn('', '', 0.1, 1)}
-        className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis
-        sapiente ipsum dolorum dicta eaque cumque inventore molestias, beatae ea
-        quaerat alias accusamus voluptas autem! Alias odit voluptates in totam
-        vitae dignissimos minus eaque culpa unde tempore dolore aperiam
-        obcaecati voluptatum aliquam corrupti, suscipit accusamus! Odit unde
-        veniam dolorum ipsum doloribus.
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]"
+      >
+        Hi, I’m Esha Amir, a dedicated Frontend Developer with a solid
+        foundation in HTML, CSS, JavaScript, and React.js. After completing my
+        degree in Computer Science, I’ve focused on building responsive,
+        user-friendly web applications with clean, maintainable code and
+        thoughtful design. I specialize in crafting smooth UI/UX experiences
+        using tools like Tailwind CSS, TypeScript, and React Router. I'm
+        constantly learning and currently diving deeper into UI/UX design
+        principles to elevate the visual and interactive quality of my work.
+        From developing dynamic single-page applications and integrating APIs to
+        improving performance and accessibility, I aim to bring both creativity
+        and functionality to every project I take on.
       </motion.p>
 
       <div className="mt-20 flex flex-wrap gap-10">
@@ -54,4 +62,4 @@ const About = () => {
   );
 };
 
-export default SectionWrapper(About, 'about');
+export default SectionWrapper(About, "about");
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,5 @@
 import {
   frontend,
-  backend,
   ux,
   prototyping,
   javascript,
@@ -13,258 +12,211 @@ import {
   nodejs,
   git,
   figma,
-  docker,
-  postgresql,
-  rubyrails,
-  graphql,
   komikult,
   leaderboard,
   math,
   movie,
-  nyeusi,
-  space,
   coverhunt,
   dcc,
   kelhel,
   microverse,
-} from '../assets';
+} from "../assets";
 
 export const navLinks = [
   {
-    id: 'about',
-    title: 'About',
+    id: "about",
+    title: "About",
   },
   {
-    id: 'projects',
-    title: 'Projects',
+    id: "projects",
+    title: "Projects",
   },
   {
-    id: 'contact',
-    title: 'Contact',
+    id: "contact",
+    title: "Contact",
   },
 ];
 
 const services = [
   {
-    title: 'Frontend Developer',
+    title: "Frontend Developer",
     icon: frontend,
   },
   {
-    title: 'Backend Developer',
-    icon: backend,
-  },
-  {
-    title: 'UI/UX Design',
+    title: "UI/UX Design",
     icon: ux,
   },
   {
-    title: 'Software Prototyping',
+    title: "Software Prototyping",
     icon: prototyping,
   },
 ];
 
 const technologies = [
   {
-    name: 'HTML 5',
+    name: "HTML 5",
     icon: html,
   },
   {
-    name: 'CSS 3',
+    name: "CSS 3",
     icon: css,
   },
   {
-    name: 'JavaScript',
+    name: "JavaScript",
     icon: javascript,
   },
   {
-    name: 'TypeScript',
+    name: "TypeScript",
     icon: typescript,
   },
   {
-    name: 'React JS',
+    name: "React JS",
     icon: reactjs,
   },
   {
-    name: 'Redux Toolkit',
+    name: "Redux Toolkit",
     icon: redux,
   },
   {
-    name: 'Tailwind CSS',
+    name: "Tailwind CSS",
     icon: tailwind,
   },
   {
-    name: 'Node JS',
+    name: "Node JS",
     icon: nodejs,
   },
   {
-    name: 'Rails',
-    icon: rubyrails,
-  },
-  {
-    name: 'graphql',
-    icon: graphql,
-  },
-  {
-    name: 'postgresql',
-    icon: postgresql,
-  },
-  {
-    name: 'git',
+    name: "git",
     icon: git,
   },
   {
-    name: 'figma',
+    name: "figma",
     icon: figma,
-  },
-  {
-    name: 'docker',
-    icon: docker,
   },
 ];
 
 const experiences = [
   {
-    title: 'Front-End Developer',
-    company_name: 'Cover Hunt',
+    title: "Front-End Developer",
+    company_name: "Forbmax",
     icon: coverhunt,
-    iconBg: '#333333',
-    date: 'Aug 2021 - Feb 2022',
+    iconBg: "#333333",
+    date: "Oct 2024 - Present",
   },
   {
-    title: 'Mentor (Volunteer)',
-    company_name: 'Microverse',
+    title: "Junior Front-End Developer intern",
+    company_name: "HA Technologies",
     icon: microverse,
-    iconBg: '#333333',
-    date: 'Mar 2022 - May 2022',
+    iconBg: "#333333",
+    date: "Aug 2023 - Nov 2023",
   },
   {
-    title: 'Junior Software Engineer',
-    company_name: 'Kelhel',
+    title: "Junior Software Engineer",
+    company_name: "Bytewise Limited",
     icon: kelhel,
-    iconBg: '#333333',
-    date: 'May 2022 - Oct 2022',
+    iconBg: "#333333",
+    date: "May 2024 - Oct 2024",
   },
   {
-    title: 'Full Stack Developer',
-    company_name: 'Diversity Cyber Council',
+    title: "Web Developer",
+    company_name: "Accenture",
     icon: dcc,
-    iconBg: '#333333',
-    date: 'Sep 2022 - Present',
+    iconBg: "#333333",
+    date: "JAN 2024 - MAY 2024",
   },
 ];
 
 const projects = [
   {
-    id: 'project-1',
-    name: 'KomiKult',
-    description: 'A comic characters list app that displays Marvel characters.',
+    id: "project-1",
+    name: "Postman clone",
+    description:
+      "A Postman-inspired web app for testing APIs with customizable HTTP requests and real-time response display.",
     tags: [
       {
-        name: 'react',
-        color: 'blue-text-gradient',
+        name: "react",
+        color: "blue-text-gradient",
       },
       {
-        name: 'mongodb',
-        color: 'green-text-gradient',
+        name: "mongodb",
+        color: "green-text-gradient",
       },
       {
-        name: 'tailwind',
-        color: 'pink-text-gradient',
+        name: "tailwind",
+        color: "pink-text-gradient",
       },
     ],
     image: komikult,
-    repo: 'https://github.com/shaqdeff/KomiKult',
-    demo: 'https://shaqdeff.github.io/KomiKult/',
+    repo: "https://github.com/eshaamir/Postman",
+    demo: "https://postman-omega.vercel.app/",
   },
   {
-    id: 'project-2',
-    name: 'Leaderboard',
+    id: "project-2",
+    name: "Gemini Chatbot",
     description:
-      'A leaderboard list app that displays scores submitted by different players.',
+      "A chatbot web app built with React.js that simulates AI-based conversations using dynamic user input handling.",
     tags: [
       {
-        name: 'react',
-        color: 'blue-text-gradient',
+        name: "react",
+        color: "blue-text-gradient",
       },
       {
-        name: 'restapi',
-        color: 'green-text-gradient',
+        name: "restapi",
+        color: "green-text-gradient",
       },
       {
-        name: 'scss',
-        color: 'pink-text-gradient',
+        name: "scss",
+        color: "pink-text-gradient",
       },
     ],
     image: leaderboard,
-    repo: 'https://github.com/shaqdeff/Leaderboard',
-    demo: 'https://shaqdeff.github.io/Leaderboard/',
+    repo: "https://github.com/eshaamir/Gemini-chatbot",
+    demo: "https://gemini-chatbot-seven-beryl.vercel.app/",
   },
   {
-    id: 'project-3',
-    name: 'Math Magicians',
-    description: 'This is a single-page calculator app built with React',
+    id: "project-3",
+    name: "Flappy Bird Game",
+    description:
+      "This is a recreation of the classic game using modern technologies.",
     tags: [
       {
-        name: 'nextjs',
-        color: 'blue-text-gradient',
+        name: "nextjs",
+        color: "blue-text-gradient",
       },
       {
-        name: 'supabase',
-        color: 'green-text-gradient',
+        name: "supabase",
+        color: "green-text-gradient",
       },
       {
-        name: 'css',
-        color: 'pink-text-gradient',
+        name: "css",
+        color: "pink-text-gradient",
       },
     ],
     image: math,
-    repo: 'https://github.com/shaqdeff/Math-Magicians',
-    demo: 'https://inspiring-medovik-37d3b3.netlify.app/',
+    repo: "https://github.com/eshaamir/Flappy-bird-Game",
+    demo: "https://flappy-bird-game-classic.vercel.app/",
   },
   {
-    id: 'project-4',
-    name: 'Movie Metro',
-    description: `A single-page application that allows users to search for any movie or show's ratings and its details.`,
+    id: "project-4",
+    name: "Tic Tac Toe",
+    description: `A simple tic-tac-toe game built using React.`,
     tags: [
       {
-        name: 'nextjs',
-        color: 'blue-text-gradient',
+        name: "nextjs",
+        color: "blue-text-gradient",
       },
       {
-        name: 'supabase',
-        color: 'green-text-gradient',
+        name: "supabase",
+        color: "green-text-gradient",
       },
       {
-        name: 'css',
-        color: 'pink-text-gradient',
+        name: "css",
+        color: "pink-text-gradient",
       },
     ],
     image: movie,
-    repo: 'https://github.com/shaqdeff/Movie-Metro',
-    demo: 'https://movie-metro.netlify.app/',
-  },
-  {
-    id: 'project-5',
-    name: 'Nyeusi Fest Site',
-    description:
-      'This is a demo concert website for a music festival called Nyeusi.',
-    tags: [
-      {
-        name: 'nextjs',
-        color: 'blue-text-gradient',
-      },
-      {
-        name: 'supabase',
-        color: 'green-text-gradient',
-      },
-      {
-        name: 'css',
-        color: 'pink-text-gradient',
-      },
-    ],
-    image: nyeusi,
-    repo: 'https://github.com/shaqdeff/Nyeusi-Fest-Site',
-    demo: 'https://shaqdeff.github.io/Nyeusi-Fest-Site/',
+    repo: "https://github.com/eshaamir/Tic-Tac-Toe",
+    demo: "https://tic-tac-toe-classic.vercel.app/play",
   },
 ];
 
